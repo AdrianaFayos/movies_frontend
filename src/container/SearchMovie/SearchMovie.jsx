0e274245a6e9128ceeefa5c173ad2c2790b0c4b0
AdrianaFayos <adrifayosperis@gmail.com>
2021-07-01 19:07:33 +0200

    search movie view works

--- src/container/SearchMovie/SearchMovie.jsx
+++ src/container/SearchMovie/SearchMovie.jsx
@@ -3,41 +3,77 @@ import UserHeader from '../../components/UserHeader/UserHeader';
 import axios from 'axios';
 import Link from '../../components/Link/Link';
 import { connect } from "react-redux";
+import { MOVIE } from "../../redux/types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList, faSearch, faUsers} from "@fortawesome/free-solid-svg-icons";
 import Logo from '../../components/Logo/Logo';
+import { useHistory } from 'react-router-dom';
+
+
 const SearchMovie = (props) => {
 
+    let history = useHistory()
 
+    //hooks 
+    const [ searchMovie, setSearchMovie] = useState({
+        movieTitle: ''
+    });
+
+    const [ movies, setMovies ] = useState([]);
 
     useEffect(() => {
-
-
-
+        
     },[]);
 
     useEffect(() => {
-
+        searchMovies ()
     });
 
+    const updateSearch = (e) => {
+        setSearchMovie({...searchMovie, [e.target.name]: e.target.value})
+    }
 
+    const searchMovies = async () => {
+        let movie = searchMovie.movieTitle;
+
+        let res = await axios.get('http://localhost:3006/movies/search/'+movie);
+
+        setMovies(res.data.results)
+
+
+    } 
+
+    const getFilmInfo = (film) => {
+        props.dispatch({ type: MOVIE, payload: film });
+
+        history.push("/filminfo");
+    }
+
+    const baseImgUrl = "https://image.tmdb.org/t/p"
+    const size = "w200"
 
     if (props.credentials?.user.isAdmin === true) {
 
         return(
-            <div className="vista">
+            <div className="searchContainer">
                 <Logo />
                 <div className="userHeaderHome">
                     <UserHeader/>
                 </div> 
+
+                <div className="movieSearch">
+                    <input type="text" name="movieTitle" id="" className="searcher" onChange={updateSearch} placeholder="Please enter a title"/>
+                    <FontAwesomeIcon icon={faSearch}/>
+                </div>
                 <div className="searchContent">
-                    <div className="movieSearch">
-                        <input type="text" name="searchMovie" id="" className="searcher"/>
-                        <FontAwesomeIcon icon={faSearch}/>
-                    </div>
-                    <div className="moviesFound">
-                        
-                    </div>
+
+                        {movies.map((film, index) => (
+        
+                            <div className="contentFilm" key={index} onClick={() => getFilmInfo(film)}>
+                               <img src={`${baseImgUrl}/${size}${film.poster_path}`} className="film1" width="180" alt="poster"/>
+                            </div>
+                    
+                        ))}
                 </div>
             </div>
         )
